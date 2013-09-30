e0b5da40a3e7571b3782b82f1a03fc717ee34573
gratefulfrog <gratefulfrog@gmail.com>
2013-09-30 17:38:07 +0200

    improved the test harness

--- tests/linux/pyserial-test.py
+++ tests/linux/pyserial-test.py
@@ -27,70 +27,96 @@ readTimeout = 0
 linvorPort = "/dev/rfcomm1"  #linvor
 smirfPort  = "/dev/rfcomm0"  #bluesmirf
 
-dataInit = ['02255',
-            '03255',
-            '09255100001225511255'
+minDelay = 0.05  # the min time to wait after a send before checking for a read
+connectDelay = 5  # when the device is opened, giveit time to start up??
+
+writeChunkSize = 5
+readChunkSize = 6
+
+dataInit = [#'02255',
+            #'03255',
+            #'09255',
+            '04255',
+            '05255',
+            '10000',
+            '12255',
+            '11255'
             ]
 
-dataCycle = ['09255' 
+dataCycle = ['09255', 
              '09000'
              ]
-cycleDelay  = 0.2
+cycleDelay  = 0.1 # the pause time between elements in the dataCycle
 
-noReadLimit = 100
+noReadLimit = 10 # how many empty reads before exiting
 
-def init(btPort = smirfPort):
+def init(btPort): # = smirfPort):
+    # keep trying to open the rfcomm device until success
+    # return the serial object
     connected = False
     while (not connected):
-        ser = serial.Serial(port=btPort,baudrate=baudRate,timeout=0) #readTimeout)
+        ser = serial.Serial(port=btPort,baudrate=baudRate,timeout=readTimeout)
         ser.open()
         connected = ser.isOpen()
         if(connected):
+            time.sleep(connectDelay)
             print 'Connected.'
         else:
             print 'Not Connected.'
     return ser
         
-def readStuff(ser, inCount):
-    something = False
-    count = inCount 
-    nothingCount = 0
-    while (something or count == inCount):
+def readStuff(ser, inCount, nbReads):
+    # read nbReads from ser obj, with the current counter at inCount, 
+    # return the new current counter if something got read (will always, or crash)
+    readLimit = readChunkSize * nbReads
+    stuff = ""
+    while (len(stuff) < readLimit):
         got = ser.read(6)
-        something = (len(got) > 0)
-        if something:
-            print str(count) + ': read: ' + got
-            count += 1
-            nothingCount = 0
-        else:
-            print 'nothing read'
-            nothingCount +=1
-        if nothingCount > noReadLimit:
-            print 'exit on nothing read'
-            exit()
-    return count
+        if len(got) > 0:
+            stuff += got 
 
-def main():
-    if len(sys.argv)>1 and  (sys.argv[1] == "linvor"):
-        ser = init(linvorPort)
-    else:
-        ser = init()
+    inc = 0
+    if stuff != "":
+        inc = 1
+    print str(inCount+inc) + ': read: ' + stuff
+    return inCount+inc
 
+def setup(serObj):
+    # sends all the elements in the dataInit, then reads the reply
+    # returns the id-number of last read
+    count = 0
+    dataWrote = ""
     for d in dataInit :
-        print str(0) + ': writing: ' + d 
-        ser.write(d)
-        time.sleep(0.2)
-    count = readStuff(ser, 0)
+        print str(count) + ': write: ' + d 
+        serObj.write(d)
+        dataWrote += d
+        count += 1
+    time.sleep(minDelay)
+    return readStuff(serObj, count, len(dataWrote)/writeChunkSize)
 
-    i = 0
+def loopForEver(serObj,currentCount):
+    # this does the infinite loop cycling the dataCycle elements
     while(True):
-        print str(count) + ': writing: ' + dataCycle[i] 
-        ser.write(dataCycle[i])
-        time.sleep(cycleDelay)
-        count = readStuff(ser, count+1)
-        i = (i + 1) % len(dataCycle)
+        for d in dataCycle:
+            print str(currentCount) + ': write: ' + d
+            serObj.write(d)
+            time.sleep(max(minDelay,cycleDelay))
+            count = readStuff(serObj, currentCount+1, len(d)/writeChunkSize) + 1
+    
+def main():
+    # first see if a BT Device argument was provided
+    port = smirfPort
+    if len(sys.argv)>1 and (sys.argv[1] == "linvor"):
+        port = linvorPort
+    
+    # now do the serial init
+    ser = init(port)
 
-
+    # then send any setup data to perpare for the cycling
+    count = setup(ser) + 1
+                
+    # then loop
+    loopForEver(ser,count)
 
 if __name__ == '__main__':
     main()
