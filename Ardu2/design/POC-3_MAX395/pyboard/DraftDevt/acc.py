5fae072b6e5fedbd16622efd8801aa55ac12a921
gratefulfrog <gratefulfrog@gmail.com>
2015-07-04 15:10:43 +0200

    accelerometer tested and operated succesfully

--- Ardu2/design/POC-3_MAX395/pyboard/DraftDevt/acc.py
+++ Ardu2/design/POC-3_MAX395/pyboard/DraftDevt/acc.py
@@ -1,63 +1,97 @@
 # acc.py
 # tests of accellerometers
+# currently seems to work with the values as they are!
 
-from pyb import Accel, LED, delay
+from pyb import Accel, LED, delay, millis
 
 red    = LED(1)
 green  = LED(2)
 yellow = LED(3)
 blue   = LED(4)
 
-
-def test(min=5):
-    a = Accel()
-    while True:
-        print (a.filtered_xyz())
-        print(a.tilt())
-        print(a.x())
-        print(a.y())
-        print(a.z())
-        delay(200)
-
-def rock(s = 10):
+def xtest(p=5,m=-5, d= 20, timeOut= 1000):
+    """
+    this uses the pyboard leds to indicate movement in the x,y,z directions.
+    x-axis: the shorter dimension of the board (BLUE led)
+    y-axis: the longer  dimension of the board (YELLOW led)
+    z-axis: vertical (GREEN led)
+    How it works:
+    1. define Zones, when the acceleration is greater than a positive threshold
+       or less than a negative threshold. These thresholds are the arguments
+       'p' and 'm',
+    2. the zones are 1 if >= pos threshold, -1 if <= neg threshold, 
+       and 0, i.e. the deadband, otherwise.
+    3. base values for the acclerations are taken to determine the 'zero'.
+    4. a vector of last times a led was changed is maintained for timeouts
+    5. the 'd' argument is the delay between accelerometer readings.
+    6. the timeOut is the time to wait before turning off a led for which 
+       there has been no activity. 
+    Loop:
+       0. wait for the appropriate time to pass,
+       1. if any new value is out of range, skip the iteration
+       2. for each axis value, 
+          0. find its zone, after subtracting the base val
+          1. If we are in a detection zone not the deadband,
+             0. if it has changed zones, then:
+                toggle led,
+                update timeout timer,
+                update last zone
+          2. if its timeout has expired, then
+             turn off the corresponding led 
+    """
     a = Accel()
     delay(50)
-    count = [0,0,0]
-    v= [a.x(),a.y(),a.z()]
+    global red,green, yellow, blue
+    leds = [[blue,0],[yellow,0],[green,0]]
+    ledsOff(leds)        
+    zp = [0,0,0]
+    zc = [0,0,0]
+    base= [a.x(),a.y(),a.z()]
+    t = millis()
+    lastActionTime = [t,t,t]
     while True:
-        x = a.x()
-        y = a.y()
-        z = a.z()
-        xd = abs(x-v[0]) > s
-        yd = abs(y-v[1]) > s
-        zd = abs(z-v[2]) > s
-        v[0]=x
-        v[1]=y
-        v[2]=z
-        if  xd and yd:
-            print("x y: " +str(count[0]))
-            count[0] +=1
-        if  xd and zd:
-            print("x z: " +str(count[1]))
-            count[1] +=1
-        if  zd and yd:
-            print("y z: " +str(count[2]))
-            count[2] +=1
-        delay(50)
+        delay(d)
+        vc= [a.x(),a.y(),a.z()]
+        if any([abs(v)>32 for v in vc]):
+            print ('continuing...')
+            continue
+        for i in range(3):
+            zc[i] = DetectionZone(vc[i]-base[i],p,m)
+            if zc[i]: # we are in a detection zone
+                if zc[i] != zp[i]:
+                    toggle(leds[i])
+                    lastActionTime[i] = millis()
+                    zp[i] = zc[i]
+            if millis()-lastActionTime[i] > timeOut:
+                off(leds[i])
+                
+def ledsOff(ls):
+    [off(l) for l in ls]
+
+def off(ldPair):
+    ldPair[0].off()
+    ldPair[1] = 0
     
-def fvn():
-    a=Accel()
-    delay(50)
-    while True:
-        f = a.filtered_xyz()
-        x = a.x()
-        y = a.y()
-        z = a.z()
-        t = a.tilt()
-        print ('filtered: ' + str(f))
-        print ('raw:      (' + str(x) + ',' + str(y) + ',' + str(z) + ')')
-        print('tilt:      '  + str(t))
-        print('----------------')
-        delay(1000)
-        
+def DetectionZone(val, posLim, negLim):
+    res = 0
+    if val >= posLim:
+        res = 1
+    elif  val <= negLim:
+        res = -1
+    return res
+
+def toggle(ldPair):
+    if ldPair[1]: # it's on, turn off
+        ldPair[0].off()
+        ldPair[1] = 0
+    else:         # it's off, turn on
+        ldPair[0].on()
+        ldPair[1] = 1
+
+def sign(x):
+    if x==0:
+        return 1
+    else:
+        return x/abs(x)
+
     
