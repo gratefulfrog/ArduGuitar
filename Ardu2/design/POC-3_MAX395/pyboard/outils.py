87822345cfabf4fa58df6ec996d74b834f95e7ff
gratefulfrog <gratefulfrog@gmail.com>
2015-07-10 14:57:47 +0200

    creation of outils.py and the RMap class to map from one range of values to another

---
+++ Ardu2/design/POC-3_MAX395/pyboard/outils.py
@@ -0,0 +1,33 @@
+# outils.py
+# provides various utility classes
+# * RMap: maps values from one range to another
+
+class RMap:
+    def __init__(self,fromRange, toRange, makeInt=False):
+        """ 
+        create an instance,
+        will raise an error if the from Range is of Zero length.
+        usage:
+        >>> m = RMap([0,100],[0,10], True) # will return ints
+        >>> m.v(8)
+        1
+        >>> n = RMap([0,100],[0,10]) # will return floats
+        >>> n.v(8)
+        0.8
+        """
+        self.ratio = (toRange[1]-toRange[0])/(fromRange[1]-fromRange[0])
+        self.fZero = fromRange[0]
+        self.tZero = toRange[0]
+        if makeInt:
+            self.typ = lambda x:round(x)
+        else:
+            self.typ = lambda x:x
+        
+
+    def v(self,val):
+        """
+        return the result of mapping val from the from range 
+        to the target range.
+        """
+        return self.typ(self.ratio*(val-self.fZero) + self.tZero)
+    
