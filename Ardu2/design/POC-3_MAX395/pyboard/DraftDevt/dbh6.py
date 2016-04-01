a876f447d8b7709a0378393985626ee06d92f611
gratefulfrog <gratefulfrog@gmail.com>
2016-04-01 19:38:16 +0200

    dbh6 ran on 6 pushbutton PCBs + HW deounce PCB - worked perfectly!

--- Ardu2/design/POC-3_MAX395/pyboard/DraftDevt/dbh6.py
+++ Ardu2/design/POC-3_MAX395/pyboard/DraftDevt/dbh6.py
@@ -2,6 +2,20 @@
 # dbh6.ph debounce hardware 
 
 """
+This is the working routine to manage 6 interrupts with hardware debouncing.
+
+How does it work?
+Overview: (with some details not discussed)
+* a vector flagVec contains ints, initially set to zero, for each pin.
+* the callback increments the flagVec[pin]
+* an endless loop checks the value of flagVec[each pin] and if >0, does the flag(pin).
+* the doFlag(pin) is only called whe flagVec[pin]>0, we want to filter out any bouncing,
+  so we only act on the first call, i.e. when flagVec[pin] == 1
+  If it is so, we print out the info (this is where we would put a call to do something),
+  we increment the global interrupt count.
+  In all cases we reset flagVec[pin]
+* the endless loop is enclosed in a try/except to allow for clean exit in case of keyboard interrupt.
+
 Pyboard:
 Switch pins, interrupt lines:
 X9  :  06 
@@ -29,7 +43,6 @@ lineDict = { 6:0, 7:1 , 8:2, 9:3,12:4, 13:5}
 flagVec= [False for i in range(nbPins)]
 interCount=0
 eVec = [None for i in range(nbPins)]
-#loopDelay = 20
 
 # define ISR
 def callback(line):
@@ -44,6 +57,7 @@ def init():
 def doFlag (ind):
     global flagVec,interCount
     if flagVec[ind] ==1:
+        # this is where to put the call to a function that actuall does something.
         print('Switch:',ind, '\tFlag: ', flagVec[ind],'\tInterCount: ',interCount)
         interCount +=1
     flagVec[ind]=0
@@ -55,7 +69,6 @@ def loop():
             if flagVec[i]>0:
                 doFlag(i)
             i = (i+1) % nbPins
-            #delay(loopDelay)
     except KeyboardInterrupt:
         print('Test ended!\nBye ...')
 
