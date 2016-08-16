10765ffe89897f366553ddffa68a9d326cb2ad36
gratefulfrog <gratefulfrog@gmail.com>
2016-08-16 13:22:02 +0200

    created Tools directory, some tools moved there, a pathSet function to allow for sd card priority importing

---
+++ Ardu2/design/POC-3_MAX395/pyboard/Tools/pathSet.py
@@ -0,0 +1,16 @@
+# pathSet.py
+# set sys.path to first import from /sd.
+
+import sys
+def setPath():
+    """
+    extend the path variable by one slot,
+    then rotate all the values one slot,
+    then set path[0] to '/sd'
+    """
+    sys.path.append(None)
+    for i in range(len(sys.path)-1,1,-1):
+        sys.path[i] = sys.path[i-1]
+    sys.path[0]='/sd'
+
+
