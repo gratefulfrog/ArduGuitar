d7892a5d57f7100e3a7bd6491addd24622bc2de6
gratefulfrog <gratefulfrog@gmail.com>
2016-08-26 12:06:30 +0200

    fixed error in pathSet.py now enables correct loading from sd-card and frozen code as well

--- Ardu2/design/POC-3_MAX395/pyboard/Tools/pathSet.py
+++ Ardu2/design/POC-3_MAX395/pyboard/Tools/pathSet.py
@@ -2,16 +2,16 @@
 # set sys.path to first import from /sd.
 
 import sys
-def setPath():
+def set():
     """
     extend the path variable by one slot,
     then rotate all the values one slot,
     then set path[0] to '/sd'
     """
     sys.path.append(None)
-    for i in range(len(sys.path)-1,1,-1):
+    for i in range(len(sys.path)-1,0,-1):
         sys.path[i] = sys.path[i-1]
     sys.path[0]='/sd'
 
-setPath()
+set()
 
